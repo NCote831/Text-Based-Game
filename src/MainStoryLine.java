094cc3bfca5f020f8027f60148ccc4f9df09d1f5
Nicholas <nic83102@gmail.com>
2021-01-10 23:40:34 -0600

    Started making .java's

---
+++ src/MainStoryLine.java
@@ -0,0 +1,6 @@
+public class MainStoryLine {
+
+    public MainStoryLine(){
+
+    }
+}
---
+++ src/mainstarter.java
@@ -0,0 +1,6 @@
+public class mainstarter {
+    public static void main(String[] args){
+        MainStoryLine mainstory = new MainStoryLine();
+        SideStoryLine sidestory = new SideStoryLine();
+    }
+}
